5a225de14ccd757254c0c6a4cd99d6b395fe76e8
Kellan Childers <Kellan Childers@gmail.com>
2016-09-17 19:16:17 -0400

    woah

--- adventure/urls.py
+++ adventure/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 app_name = 'adventure'
 urlpatterns = [
+    url(r'^login/?$', views.login, name="login")
 ]
--- adventure/views.py
+++ adventure/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
 from django.db.models import F
+from django.contrib.auth import authenticate, login
 
+
+def login(request, test):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page.
+        else:
+            # Return a 'disabled account' error message
+            pass
+    else:
+        # Return an 'invalid login' error message.
+        pass
+    return render(request, 'adventure/test.html')
