b2ce16746ab9e99787d85cd388cdb6512c7e5467
Doctor-Gandalf <Kellan Childers@gmail.com>
2016-09-17 23:36:10 -0400

    removed address foreign key in event

--- adventure/models.py
+++ adventure/models.py
@@ -19,7 +19,8 @@ class Event(models.Model):
     name = models.CharField(max_length=200, default='New Event')
     description = models.CharField(max_length=2000, blank=True)
     date = models.DateTimeField('date hosted')
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    # address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
     @python_2_unicode_compatible
     def __str__(self):
         return str(self.name)
